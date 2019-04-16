809b48e1b830899cd4af26457b5b30dca4ad0093
MilanSuvajdzic <milansuvajdzic24@gmail.com>
2019-04-16 21:59:24 +0200

    radi kontejneer sa inmemory

--- uks-2019-master/start.sh
+++ uks-2019-master/start.sh
@@ -3,7 +3,7 @@
 # start django
 python manage.py collectstatic --noinput
 python manage.py makemigrations
-python manage.py migrate
-python manage.py test vsc
+python manage.py migrate --run-syncdb
+#python manage.py test vsc
 #python manage.py runserver
-gunicorn vsc_app.wsgi -b 0.0.0.0:8000
+gunicorn djangoproject.wsgi -b 0.0.0.0:8000
